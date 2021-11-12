363aa8f98b1e400f7bbc98c386c312588d3cfb0d
Jose V. Trigueros <j.v.trigueros@gmail.com>
2021-11-12 08:59:16 -0700

    Remove test directory, no tests

--- test/dbl_server_stats/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns dbl-server-count.core-test
-  (:require [clojure.test :refer :all]
-            [dbl-server-count.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
